bf6b7719d6a8c370a750a149e9c047937f027830
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-12 22:05:55 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, 4_digit_display.ts

--- 4_digit_display.ts
+++ 4_digit_display.ts
@@ -44,11 +44,11 @@ namespace pins {/* 4_digit_display.ts
     export function segmente_anzeigen(seg_byte: number, stelle: number) {
         // 76543210 seg_byte: Punkt p und 7 Segmente a..g 
         // pgfedcba
-        let displayIndex = (stelle >> 2)
+        let displayIndex = (stelle >> 2) * 2
         if (displayIndex + 1 < qDisplayPins.length && seg_byte >= 0x00 && seg_byte <= 0xFF) {
 
-            let clkPin: DigitalPin = qDisplayPins[displayIndex * 2]
-            let dataPin: DigitalPin = qDisplayPins[displayIndex * 2 + 1]
+            let clkPin: DigitalPin = qDisplayPins[displayIndex]
+            let dataPin: DigitalPin = qDisplayPins[displayIndex + 1]
 
             start(clkPin, dataPin)
             writeByte(0x44, clkPin, dataPin)
--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    pins.clear()
+})
 if (!(pins.simulator())) {
     pins.createDisplay(DigitalPin.C16, DigitalPin.C17)
     pins.clear()
     pins.segmente_anzeigen(7, 0)
     pins.segmente_anzeigen(130, 6)
+    pins.segmente_anzeigen(137, 11)
 }
