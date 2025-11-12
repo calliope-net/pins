c317a1624f3c2100183b58de949f1821078c0ae4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-12 19:48:27 +0100

    Aktualisiere pxt.json, main.blocks, 4_digit_display.ts

---
+++ 4_digit_display.ts
@@ -0,0 +1,13 @@
+
+namespace pins {/* 4_digit_display.ts
+
+*/
+    //% group="Grove - 4-Digit Display" subcategory="4-Digit Display"
+    //% block="beim Start Takt %clkPin Daten %dataPin" weight=9
+    //% clkPin.defl=DigitalPins.C16 dataPin.defl=DigitalPins.C17
+    export function createDisplay(clkPin: DigitalPin, dataPin: DigitalPin) {
+      
+    }
+
+
+} // 4_digit_display.ts
