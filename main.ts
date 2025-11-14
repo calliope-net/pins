14c1e426785d31cdfae8bfb66e33d56f44a2b8bf
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-14 19:22:01 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-	
-})
-if (!(pins.simulator())) {
-    pins.addDisplay(pins.pins_DigitalPin(DigitalPin.C16), pins.pins_DigitalPin(DigitalPin.C17))
-    pins.addDisplay(pins.pins_DigitalPin(DigitalPin.P2), pins.pins_DigitalPin(DigitalPin.P3))
-    pins.addDisplay(pins.pins_DigitalPin(DigitalPin.P0), pins.pins_DigitalPin(DigitalPin.P1))
-}
+
