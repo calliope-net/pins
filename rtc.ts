64dde2507b8bc514f3028bba01449cb4c2538fda
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-20 11:14:14 +0100

    Aktualisiere main.blocks, rtc.ts

--- rtc.ts
+++ rtc.ts
@@ -23,7 +23,7 @@ CMOS Real-Time Clock (RTC) - Quarz-Uhr mit Knopfzelle CR1225 3Volt
     export function pins_rtc_eRegister(pRegister: rtc_eRegister): number { return pRegister }
 
 
-
+    // ========== group="Real Time Clock PCF85063TP" subcategory="RTC Uhr"
 
     //% group="Real Time Clock PCF85063TP" subcategory="RTC Uhr"
     //% block="Datum und Zeit einlesen" weight=9
@@ -39,6 +39,10 @@ CMOS Real-Time Clock (RTC) - Quarz-Uhr mit Knopfzelle CR1225 3Volt
         else
             return []
     }
+
+
+
+    // ========== group="Uhr lesen (vorher 'Datum und Zeit einlesen')" subcategory="RTC Uhr"
 
     //% group="Uhr lesen (vorher 'Datum und Zeit einlesen')" subcategory="RTC Uhr"
     //% block="%register als Zahl" weight=8
@@ -62,7 +66,7 @@ CMOS Real-Time Clock (RTC) - Quarz-Uhr mit Knopfzelle CR1225 3Volt
     }
 
     //% group="Uhr lesen (vorher 'Datum und Zeit einlesen')" subcategory="RTC Uhr"
-    //% block="Zeit als Text" weight=6
+    //% block="Zeit als Text" weight=4
     export function rtc_get_time(): string {
         // time_string = str(RTC_BUFFER[2] >> 4) + str(RTC_BUFFER[2] & 0x0F) + ":" + str(RTC_BUFFER[1] >> 4) + str(RTC_BUFFER[1] & 0x0F) + ":" + str(RTC_BUFFER[0] >> 4) + str(RTC_BUFFER[0] & 0x0F)
         if (rtc_Buffer)
@@ -71,5 +75,40 @@ CMOS Real-Time Clock (RTC) - Quarz-Uhr mit Knopfzelle CR1225 3Volt
             return ""
     }
 
+    export enum rtc_eFormat {
+        //% block="dd.MM.yy"
+        ddMMyy,
+        //% block="dd.MM.20yy"
+        ddMM20yy,
+        //% block="ddd (Wochentag)"
+        ddd,
+        //% block="HH:mm"
+        hhmm,
+        //% block="HH:mm:ss"
+        hhmss
+    }
+
+
+    //% group="Uhr lesen (vorher 'Datum und Zeit einlesen')" subcategory="RTC Uhr"
+    //% block="%format als Text" weight=3
+    export function rtc_get(format: rtc_eFormat) {
+        if (rtc_Buffer)
+            switch (format) {
+                case rtc_eFormat.ddMMyy:
+                    return (rtc_Buffer[3] >> 4) + (rtc_Buffer[3] & 0x0F) + "." + (rtc_Buffer[5] >> 4) + (rtc_Buffer[5] & 0x0F) + "." + (rtc_Buffer[6] >> 4) + (rtc_Buffer[6] & 0x0F)
+                case rtc_eFormat.ddMM20yy:
+                    return (rtc_Buffer[3] >> 4) + (rtc_Buffer[3] & 0x0F) + "." + (rtc_Buffer[5] >> 4) + (rtc_Buffer[5] & 0x0F) + ".20" + (rtc_Buffer[6] >> 4) + (rtc_Buffer[6] & 0x0F)
+                case rtc_eFormat.ddd:  //wd_string = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'][int(RTC_BUFFER[4])]
+                    return ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', '-7'][rtc_Buffer[4] & 0x07]
+                case rtc_eFormat.hhmm:
+                    return (rtc_Buffer[2] >> 4) + (rtc_Buffer[2] & 0x0F) + ":" + (rtc_Buffer[1] >> 4) + (rtc_Buffer[1] & 0x0F) + ":" + (rtc_Buffer[0] >> 4) + (rtc_Buffer[0] & 0x0F)
+                case rtc_eFormat.hhmss:
+                    return (rtc_Buffer[2] >> 4) + (rtc_Buffer[2] & 0x0F) + ":" + (rtc_Buffer[1] >> 4) + (rtc_Buffer[1] & 0x0F)
+                default:
+                    return ""
+            }
+        else
+            return ""
+    }
 
 } // rtc.ts
