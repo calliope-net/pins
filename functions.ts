37503d4c4f41e7a7dcc3d965605308b1d175a79c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-21 09:40:56 +0100

    Aktualisiere functions.ts

--- functions.ts
+++ functions.ts
@@ -2,42 +2,37 @@
 namespace pins { // functions.ts
 
 
-
-    // ========== group="Funktionen" subcategory=I²C
+    // ========== group="Kommentar" subcategory=Funktionen
 
     //% blockId=pins_text block="%s" blockHidden=true
     export function pins_text(s: string): string { return s }
 
-    //% group="Funktionen" subcategory=Funktionen
+    //% group="Kommentar" subcategory=Funktionen
     //% block="// %text" weight=9
     //% text.shadow="pins_text"
     export function comment(text: any): void { }
 
 
-    //% group="Funktionen" subcategory=Funktionen
+
+    // ========== group="Logik" subcategory=Funktionen
+
+    //% group="Logik" subcategory=Funktionen
     //% block="Simulator" weight=7
     export function simulator() {
         return "€".charCodeAt(0) == 8364
     }
 
-    //% group="Funktionen" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen
     //% block="%i0 zwischen %i1 und %i2" weight=6
     export function between(i0: number, i1: number, i2: number): boolean { return (i0 >= i1 && i0 <= i2) }
 
-
-    //% group="Funktionen" subcategory=Funktionen
-    //% block="roundWithPrecision number %x digits %digits" weight=5
-    //% digits.min=0 digits.max=4 digits.defl=2
-    export function roundWithPrecision(x: number, digits: number) { return Math.roundWithPrecision(x, digits) }
-
-
-    //% group="Funktionen" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen
     //% block="%variable === undefined" weight=3
     export function isundefined(variable: any): boolean {
         return (variable === undefined)
     }
 
-    //% group="Funktionen" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen
     //% block="Number.isNaN(%variable)" weight=2
     export function isnan(variable: any): boolean {
         return Number.isNaN(variable)
@@ -45,10 +40,19 @@ namespace pins { // functions.ts
 
 
 
+    // ========== group="Mathematik" subcategory=Funktionen
+
+    //% group="Mathematik" subcategory=Funktionen
+    //% block="roundWithPrecision number %x digits %digits" weight=5
+    //% digits.min=0 digits.max=4 digits.defl=2
+    export function roundWithPrecision(x: number, digits: number) { return Math.roundWithPrecision(x, digits) }
+
+
+
     // ========== group="25 LED Matrix" subcategory=Funktionen
 
     //% group="25 LED Matrix" subcategory=Funktionen
-    //% block="25 LED x(0..3)→ %x y(0..31)↑ %n" weight=5
+    //% block="25 LED x→0..3 %x y↑0..31 %n" weight=5
     //% x.min=0 x.max=4 y.min=0 y.max=31
     export function plot25LED(x: number, n: number) {
         if (between(x, 0, 4)) {
