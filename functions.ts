eb7a728a000d64fa54e9a2b4d77b2068e92bf5e3
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-12-13 20:53:29 +0100

    Aktualisiere main.blocks, functions.ts

--- functions.ts
+++ functions.ts
@@ -7,7 +7,7 @@ namespace pins { // functions.ts
     //% blockId=pins_text block="%s" blockHidden=true
     export function pins_text(s: string): string { return s }
 
-    //% group="Kommentar" subcategory=Funktionen
+    //% group="Kommentar" subcategory=Funktionen color=#004FCF
     //% block="// %text" weight=9
     //% text.shadow="pins_text"
     export function comment(text: any): void { }
@@ -16,23 +16,23 @@ namespace pins { // functions.ts
 
     // ========== group="Logik" subcategory=Funktionen
 
-    //% group="Logik" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen color=#004FCF
     //% block="Simulator" weight=7
     export function simulator() {
         return "€".charCodeAt(0) == 8364
     }
 
-    //% group="Logik" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen color=#004FCF
     //% block="%i0 zwischen %i1 und %i2" weight=6
     export function between(i0: number, i1: number, i2: number): boolean { return (i0 >= i1 && i0 <= i2) }
 
-    //% group="Logik" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen color=#004FCF
     //% block="%variable === undefined" weight=3
     export function isundefined(variable: any): boolean {
         return (variable === undefined)
     }
 
-    //% group="Logik" subcategory=Funktionen
+    //% group="Logik" subcategory=Funktionen color=#004FCF
     //% block="Number.isNaN(%variable)" weight=2
     export function isnan(variable: any): boolean {
         return Number.isNaN(variable)
@@ -43,30 +43,30 @@ namespace pins { // functions.ts
     // ========== group="Mathematik" subcategory=Funktionen
 
     //% blockId=pins_hex4
-    //% group="Mathematik" subcategory=Funktionen weight=8
-    //% block="%x0"
+    //% group="Mathematik" subcategory=Funktionen color=#004FCF
+    //% block="%x0" weight=8
     export function hex4(x0: eHEX4bit) { return (x0 & 0xF) }
 
     //% blockId=pins_hex44
-    //% group="Mathematik" subcategory=Funktionen weight=8
-    //% block="%x1 %x0"
+    //% group="Mathematik" subcategory=Funktionen color=#004FCF
+    //% block="%x1 %x0" weight=7
     export function hex44(x1: eHEX4bit, x0: eHEX4bit) { return (x1 << 4) | (x0 & 0xF) }
 
 
-    //% group="Mathematik" subcategory=Funktionen
+    //% group="Mathematik" subcategory=Funktionen color=#004FCF
     //% block="charCodeAt %text index %index" weight=6
     export function charCodeAt(text: string, index: number) {
         return text.charCodeAt(index)
     }
 
-    //% group="Mathematik" subcategory=Funktionen
+    //% group="Mathematik" subcategory=Funktionen color=#004FCF
     //% block="parseInt %text || radix %radix" weight=4
     //% radix.min=2 radix.max=16 radix.defl=10
     export function parseint(text: string, radix?: number) {
         return parseInt(text, radix)
     }
 
-    //% group="Mathematik" subcategory=Funktionen
+    //% group="Mathematik" subcategory=Funktionen color=#004FCF
     //% block="roundWithPrecision number %x digits %digits" weight=2
     //% digits.min=0 digits.max=4 digits.defl=2
     export function roundWithPrecision(x: number, digits: number) { return Math.roundWithPrecision(x, digits) }
@@ -75,7 +75,7 @@ namespace pins { // functions.ts
 
     // ========== group="25 LED Matrix" subcategory=Funktionen
 
-    //% group="25 LED Matrix" subcategory=Funktionen
+    //% group="25 LED Matrix" subcategory=Funktionen color=#004FCF
     //% block="25 LED x→0..3 %x y↑0..31 %n" weight=5
     //% x.min=0 x.max=4 y.min=0 y.max=31
     export function plot25LED(x: number, n: number) {
