2f48c50823b55de0db825062bb126fcf864ce069
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-04-06 19:02:12 +0200

    Aktualisiere pxt.json, oled.ts

---
+++ oled.ts
@@ -0,0 +1,20 @@
+
+namespace pins // oled.ts
+/* 260406 Lutz Elßner
+Version ohne RAM, schreibt Text direkt auf das Display
+I²C Devices:
+https://wiki.seeedstudio.com/Grove-OLED-Display-1.12-SH1107_V3.0/
+https://wiki.seeedstudio.com/Grove-OLED-Yellow&Blue-Display-0.96-SSD1315_V1.0/
+https://wiki.seeedstudio.com/Grove-OLED_Display_0.96inch/
+https://www.sparkfun.com/sparkfun-qwiic-eeprom-breakout-512kbit.html
+Datenblatt für alle Displays:
+https://files.seeedstudio.com/wiki/Grove-OLED-Display-1.12-(SH1107)_V3.0/res/SH1107V2.1.pdf
+
+*/ {
+
+
+
+
+
+
+} // oled.ts
